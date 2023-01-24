9b50fa0f7a60030a30ed0457acccb77cb30f3896
Thiago Dupim <thiagodupim3@gmail.com>
2023-01-24 16:57:17 -0300

    Adição de um botão para ir para página de tarefas na barra de navegação e deixando o formulário mais estilizado com bootstrap

--- Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
+++ Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel;
+
 namespace Tarafas.Web.Models;
 
 public class TarefaViewModel
 {
+    [DisplayName("Título")]
     public string Titulo { get; set;}
+    [DisplayName("Descrição")]
     public string Descricao { get; set;}
 }
